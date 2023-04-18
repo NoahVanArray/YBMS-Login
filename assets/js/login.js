47a779dcc29740815e1cb97052c0cf80619a9648
yoe <johnyohannavarra@gmail.com>
2023-04-18 16:41:49 +0800

    a

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+/*
+	Author: John Yohan J. Navarra
+*/
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> {
+	wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+	wrapper.classList.remove('active');
+});
+
