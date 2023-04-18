c0266f774a6bf953cf01c60e78187c3ab456e374
yoe <johnyohannavarra@gmail.com>
2023-04-18 17:22:10 +0800

    a

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,6 +5,8 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
 	wrapper.classList.add('active');
@@ -14,3 +16,10 @@ loginLink.addEventListener('click', ()=> {
 	wrapper.classList.remove('active');
 });
 
+btnPopup.addEventListener('click', ()=> {
+	wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+	wrapper.classList.remove('active-popup');
+});
